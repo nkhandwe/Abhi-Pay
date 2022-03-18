370d18c43bc26e58abcf68bb4bb17e3133092b5c
nkhandwe <nkhandwe@gmail.com>
2022-03-18 09:15:08 +0530

    First Commit

---
+++ app/src/main/java/com/nktech/abhipay/MainActivity.java
@@ -0,0 +1,17 @@
+package com.nktech.abhipay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/* Created By: Nkhandwe
+On : 18.03.2022 */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
